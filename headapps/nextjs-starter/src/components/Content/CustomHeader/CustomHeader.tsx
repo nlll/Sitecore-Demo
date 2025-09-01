ea5f33cbcfd7b8ea79bd0806abf3c05256d38b18
Antoaneta Ivanova <aivanova@softserveinc.com>
2025-09-01 08:43:12 +0300

    adding language bar

--- headapps/nextjs-starter/src/components/Content/CustomHeader/CustomHeader.tsx
+++ headapps/nextjs-starter/src/components/Content/CustomHeader/CustomHeader.tsx
@@ -52,12 +52,32 @@ interface CustomHeaderProps {
 const LOGO_IMAGE_SRC = '/TireHub_Logo.png';
 const LOGO_ALT_TEXT = 'TireHub';
 
+
 export const Default = (props: CustomHeaderProps): JSX.Element => {
   const { sitecoreContext } = useSitecoreContext();
-console.log("CTA:", props.fields.data.item);
+
   return (
-    <header className={cx('container', styles.root)}>
-      <nav className={styles.nav}>
+    <header  className={cx('container', styles.root)}>
+      <div className={styles.languageContainer}>
+         <span className={styles.iconSignLanguage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="icon-size"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+            />
+          </svg>
+        </span>
+         <span className={styles.languageText}>ENGLISH [US]</span>
+      </div>
+      <nav className={styles.nav}>  
         <div className={cx(styles.primaryNav)}>
            {props.fields.data?.item.logoLink ? (
           <a href="/" className={styles.logoLink}>
@@ -108,13 +128,9 @@ console.log("CTA:", props.fields.data.item);
         )}
           {props.fields.data.item.cta && (
             <Link field={props.fields.data.item.cta.jsonValue} className={styles.ctaButton} />
-            
           )}
-          
           {props.fields.data.item.cta2 && (
-          
             <Link field={props.fields.data.item.cta2.jsonValue} className={styles.ctaButton} />
-            
           )}
           </div>
       </nav>
